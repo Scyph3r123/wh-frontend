23c6d243e8fb3a895bd1c629ecfa1475457ae3d2
Deme <demeyodaniel0@gmail.com>
2024-06-03 12:11:08 +0530

    env

--- src/apiPath.js
+++ src/apiPath.js
@@ -1,5 +1,5 @@
 const isProduction = import.meta.env.MODE === 'production';
 
-const apiPath = isProduction ? 'manage.winterhymns.com/graphql' : 'http://localhost:1337/graphql';
+const apiPath = isProduction ? 'https://manage.winterhymns.com/graphql' : 'http://localhost:1337/graphql';
 
 export default apiPath;
